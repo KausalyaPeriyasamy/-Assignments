1ec257e8fe2adeea3e9e873d225cd6a83f84ed63
KausalyaPeriyasamy <perkausi20@gmail.com>
2024-07-26 12:58:22 +0530

    Add files via upload
    Assignment 1

---
+++ PrintEvenNumbers.java
@@ -0,0 +1,18 @@
+package assignments;
+
+public class PrintEvenNumbers {
+
+	public static void main(String[] args) {
+		for(int i=2;i<=10;i+=2)
+		{
+			if(i%2==0)
+			{
+				System.out.println("The even numbers are:"+i);
+			}
+
+		}
+
+
+	}
+
+}
